aa49e044d27aff0c6026461709b13cf88db06b01
Frildoren <yinxro@gmail.com>
2013-05-24 14:30:53 +0200

    .

--- ConcurrenciaProject/src/GestorDeEventosMonitor.java
+++ ConcurrenciaProject/src/GestorDeEventosMonitor.java
@@ -1,30 +1,44 @@
 import java.util.List;
 import java.util.ArrayList;
+import es.upm.babel.cclib.*;
 
 public class GestorDeEventosMonitor implements GestorDeEventos {
 	
-	List<List<Integer>> processListeners = new ArrayList<List<Integer>>(N_OBSERVADORES);
-	@Override
-	public void emitir(int eid) {
-		// TODO Auto-generated method stub
+	List<List<Integer>> eventListeners;
+	
+	public GestorDeEventosMonitor(){
+		eventListeners = new ArrayList<List<Integer>>(N_OBSERVADORES);
 		
+		for( int i=0; i<N_OBSERVADORES; i++ ){
+			eventListeners.add(i, new ArrayList<Integer>());
+		}
+	}
+	
+	@Override
+	public void emitir(int eid){
+		List<Integer> eventList = eventListeners.get(eid);
+		
+		for( int pid : eventList ){
+			// Depsertar a pid.
+			
+		}
 	}
 
 	@Override
 	public void subscribir(int pid, int eid) {
-		// TODO Auto-generated method stub
-		
+		List<Integer> eventList = eventListeners.get(eid);
+		eventList.add(pid);
 	}
 
 	@Override
 	public void desubscribir(int pid, int eid) {
-		// TODO Auto-generated method stub
-		
+		List<Integer> eventList = eventListeners.get(eid);
+		eventList.remove(pid);
 	}
 
 	@Override
 	public int escuchar(int pid) {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
