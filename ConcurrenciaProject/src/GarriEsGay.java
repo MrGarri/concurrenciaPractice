61fea67a1417deb90e1fbee7d43ed074efc237b1
Frildoren <yinxro@gmail.com>
2013-05-24 12:13:05 +0200

    .

--- ConcurrenciaProject/src/GarriEsGay.java
+++ ConcurrenciaProject/src/GarriEsGay.java
@@ -4,6 +4,7 @@ import javax.swing.JOptionPane;
 public class GarriEsGay {
 	
 	public static void main (String args[]) {
+		// Si.
 		JOptionPane.showMessageDialog(null, "Garri es gayer", "Condición de Garri", JOptionPane.INFORMATION_MESSAGE);
 	}
 }
