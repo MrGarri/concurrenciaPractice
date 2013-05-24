f75698de2fafca18d4497b3fd59754d2733aafc4
MrGarri <garri@PortatilGarri>
2013-05-24 13:11:27 +0200

    .

--- ConcurrenciaProject/src/GestorDeEventosMonitor.java
+++ ConcurrenciaProject/src/GestorDeEventosMonitor.java
@@ -1,4 +1,31 @@
+import java.util.List;
+import java.util.ArrayList;
 
-public class GestorDeEventosMonitor {
+public class GestorDeEventosMonitor implements GestorDeEventos {
+	
+	List<List<Integer>> processListeners = new ArrayList<List<Integer>>(N_OBSERVADORES);
+	@Override
+	public void emitir(int eid) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void subscribir(int pid, int eid) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void desubscribir(int pid, int eid) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int escuchar(int pid) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
