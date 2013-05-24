1af43c815b8bda21747c9d781a4a0ca815a8c454
MrGarri <garri@PortatilGarri>
2013-05-24 12:10:09 +0200

    .

--- ConcurrenciaProject/src/GarriEsGay.java
+++ ConcurrenciaProject/src/GarriEsGay.java
@@ -1,4 +1,9 @@
+import javax.swing.JOptionPane;
+
 
 public class GarriEsGay {
-
+	
+	public static void main (String args[]) {
+		JOptionPane.showMessageDialog(null, "Garri es gayer", "Condición de Garri", JOptionPane.INFORMATION_MESSAGE);
+	}
 }
