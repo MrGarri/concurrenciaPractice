0d85dcdac026af63d6f6743e080c54e2eb88e51f
Frildoren <yinxro@gmail.com>
2013-05-25 14:22:14 +0200

    update

--- ConcurrenciaProject/src/GestorDeEventosMonitor.java
+++ ConcurrenciaProject/src/GestorDeEventosMonitor.java
@@ -1,45 +1,74 @@
-import java.util.List;
 import java.util.ArrayList;
-import es.upm.babel.cclib.*;
+import java.util.List;
+
+import es.upm.babel.cclib.Monitor;
+import es.upm.babel.cclib.Monitor.Cond;
 
 public class GestorDeEventosMonitor implements GestorDeEventos {
 	
-	List<List<Integer>> eventListeners;
+	Monitor monitor;
+	Cond[] observerConditions;
+	List<ArrayList<Integer>> eventSubscriptors;
+	
+	int eventTrigger;
 	
 	public GestorDeEventosMonitor(){
-		eventListeners = new ArrayList<List<Integer>>(N_OBSERVADORES);
+		monitor = new Monitor();
+		observerConditions = new Cond[ N_OBSERVADORES + 1 ];
+		eventSubscriptors = new ArrayList<ArrayList<Integer>>();
 		
-		for( int i=0; i<N_OBSERVADORES; i++ ){
-			eventListeners.add(i, new ArrayList<Integer>());
+		for(int i=0; i<=observerConditions.length; i++){
+			observerConditions[i] = monitor.newCond();
+		}
+		
+		for(int i=0; i<=N_EVENTOS; i++){
+			eventSubscriptors.add( i, new ArrayList<Integer>() );
 		}
 	}
 	
 	@Override
 	public void emitir(int eid){
-		List<Integer> eventList = eventListeners.get(eid);
+		monitor.enter();
 		
-		for( int pid : eventList ){
-			// Depsertar a pid.
-			
+		eventTrigger = eid;
+		List<Integer> subscriptors = eventSubscriptors.get(eid);
+		
+		for( int pid : subscriptors ){
+			observerConditions[pid].signal();
 		}
+		
+		monitor.leave();
 	}
 
 	@Override
 	public void subscribir(int pid, int eid) {
-		List<Integer> eventList = eventListeners.get(eid);
-		eventList.add(pid);
+		monitor.enter();
+		
+		List<Integer> subscriptors = eventSubscriptors.get(eid);
+		subscriptors.add(pid);
+		
+		monitor.leave();
 	}
 
 	@Override
 	public void desubscribir(int pid, int eid) {
-		List<Integer> eventList = eventListeners.get(eid);
-		eventList.remove(pid);
+		monitor.enter();
+		
+		List<Integer> subscriptors = eventSubscriptors.get(eid);
+		subscriptors.remove(pid);
+		
+		monitor.leave();
 	}
 
 	@Override
 	public int escuchar(int pid) {
+		observerConditions[pid].await();
 		
-		return 0;
+		monitor.enter();
+		int res = eventTrigger;
+		monitor.leave();
+		
+		return res;
 	}
 
 }
