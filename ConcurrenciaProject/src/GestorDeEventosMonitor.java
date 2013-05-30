56fe88d746b5cb88eee07d9ec51fe46cdd0a8fe0
MrGarri <garri@PortatilGarri>
2013-05-30 10:17:03 +0200

    Add comments

--- ConcurrenciaProject/src/GestorDeEventosMonitor.java
+++ ConcurrenciaProject/src/GestorDeEventosMonitor.java
@@ -20,6 +20,12 @@ public class GestorDeEventosMonitor implements GestorDeEventos {
 		}
 	}
 	
+	/**
+	 * <b>void</b> emitir (<b>int</b> eid)
+	 * 
+	 * <p>Emite el evento con id eid.
+	 * Los procesos suscritos a este evento pasarán a escucharlo.</p>
+	 */
 	@Override
 	public void emitir(int eid){
 		monitor.enter();
@@ -34,6 +40,13 @@ public class GestorDeEventosMonitor implements GestorDeEventos {
 		monitor.leave();
 	}
 
+	
+	/**
+	 * <b>void</b> suscribir (<b>int</b> pid, <b>int</b> eid)
+	 * 
+	 * <p>Pone a true el elemento en la matriz en la posición (eid, pid).
+	 * Monitor asegura la exclusión mutua. </p>
+	 */
 	@Override
 	public void subscribir(int pid, int eid) {
 		monitor.enter();
@@ -43,6 +56,13 @@ public class GestorDeEventosMonitor implements GestorDeEventos {
 		monitor.leave();
 	}
 
+	
+	/**
+	 * <b>void</b> desuscribir (<b>int</b> pid, <b>int</b> eid)
+	 * 
+	 * <p>Pone a false el elemento en la matriz en la posición (eid, pid)
+	 * Monitor asegura la exclusión mutua</p>
+	 */
 	@Override
 	public void desubscribir(int pid, int eid) {
 		monitor.enter();
@@ -53,7 +73,13 @@ public class GestorDeEventosMonitor implements GestorDeEventos {
 		monitor.leave();
 	}
 
-	@Override
+	/**
+	 * <b>int</b> escuchar (<b>int</b> pid).
+	 * 
+	 * <p>Se devuelve la id del evento escuchado. El evento puede haber sido emitido antes 
+	 * de ejecutar este método. En caso contrario, el proceso se queda esperando hasta que
+	 * lo sea.</p>  
+	 */
 	public int escuchar(int pid) {
 		
 		try{
