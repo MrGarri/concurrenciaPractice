b18204d7e4cae5a21ddbf97ee59cebdab4d038f3
Frildoren <yinxro@gmail.com>
2013-05-30 11:22:05 +0200

    .

--- ConcurrenciaProject/src/GestorDeEventosCSP.java
+++ ConcurrenciaProject/src/GestorDeEventosCSP.java
@@ -1,4 +1,29 @@
+import org.jcsp.*;
 
-public class GestorDeEventosCSP {
+public class GestorDeEventosCSP implements GestorDeEventos {
+
+	@Override
+	public void emitir(int eid) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void subscribir(int pid, int eid) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void desubscribir(int pid, int eid) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int escuchar(int pid) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
